af8ef73ea7ebdfec92002ce1020a91533d4e1ebc
Star <874240697@qq.com>
2020-07-16 08:02:55 +0800

    修改查看角色页面

--- src/views/RoleShow.vue
+++ src/views/RoleShow.vue
@@ -1,153 +1,170 @@
 <template>
-  <div>
-    <div class="welcomePart">
-      <label class="back" @click="$router.back()">返回</label>
-      <h2>角色信息</h2>
-      <h4>您的角色是：<span style="color: red">{{players[0].roleName}}</span></h4>
+  <div class="main">
+    <div class="header">
+      <h2 class="roleinfo">角色展示</h2>
     </div>
-    <div class="roleIntroduction">
-      <p v-html="roleIntro">
-        <!--          {{roleIntro}}-->
-      </p>
+    <div class="showotherplayer">
+      <div v-for="player in otherPlayers" :key="player.username" class="singlePlayer">
+        <img src="../assets/people.png" :class="{died: player.rolestatus===1}">
+        <label>{{player.username}}</label>
+      </div>
     </div>
-    <div class="gameState">
-      <span>游戏状态：</span><span id="state">{{playerState}}</span>
+    <div class="showcurrentplayer" v-if="currentPlayer.rolestatus==0 || currentPlayer.rolestatus==1">
+      <img :src="currentPlayer.playerimage">
+      <div class="right">
+        <span>你的身份:<label class="rolename">【 {{ currentPlayer.rolename }} 】</label></span>
+        <div>
+          <div class="status">
+            <van-divider :style="{ color: '#14aea3', borderColor: '#ffffff', marginTop: '10px', marginBottom: '10px'}">游戏状态</van-divider>
+            <label v-if="currentPlayer.rolestatus===0">游戏中</label>
+            <label v-else>已死亡</label>
+          </div>
+          <van-divider :style="{ color: '#26c622', borderColor: '#ffffff', marginTop: '10px', marginBottom: '10px'}">角色技能</van-divider>
+          <div>
+            <span class="skill">【技能】：{{currentPlayer.playerskill}}</span>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="operatBtn">
-      <!--        根据玩家状态显示可执行的操作-->
-      <button v-if="gameState=='2'" @click="refresh">状态刷新</button>
-      <button v-if="gameState=='0'" @click="lookResult">查看结果</button>
-      <button @click="outRoom">退出房间</button>
+    <div v-else class="preparing">
+      <van-loading type="spinner" size="50">等待其他玩家进入</van-loading>
+    </div>
+    <div class="bottom">
+      <van-button color="linear-gradient(to right, #6149f6, #4bb0ff)">刷新状态</van-button>
+      <van-button color="linear-gradient(to right, #4bb0ff, #6149f6)">退出房间</van-button>
     </div>
   </div>
 </template>
 
 <script>
-  import { MessageBox } from 'mint-ui';
-  export default {
-    data: function () {
-      return {
-        players: this.$store.state.Players, //玩家信息
-        gameState: this.$store.state.playerData.role_sta || 0, //玩家游戏状态
-        roleIntro: '',
-        playerState: "未开始",
-        roleName:"村民"
-      }
-    },
-    methods: {
-      //状态更新
-      refresh() {
-        //刷新角色状态，向服务器发送请求
-        this.$store.dispatch('refreshState');
-        if (this.gameState === 2) {
-          this.playerState = "存活"
-        } else if (this.gameState === 3) {
-          this.playerState = "死亡"
-        }else if (this.gameState === 0){
-          this.playerState = "未开始"
-        }else if (this.gameState === 1) {
-          this.playerState = "已结束"
+    export default {
+        name: "RoleShow.vue",
+        data: function () {
+            return {
+                currentPlayer: {
+                    rolestatus: 3,
+                    rolename: '预言家',
+                    playerimage: '',
+                    playerskill: ''
+                },
+                otherPlayers: [
+                    { username: 'aaa', rolestatus: 0},
+                    { username: 'bbb', rolestatus: 0},
+                    { username: 'eee', rolestatus: 1},
+                    { username: 'aaa', rolestatus: 0},
+                    { username: 'bbb', rolestatus: 0},
+                    { username: 'eee', rolestatus: 1}
+                ]
+            }
+        },
+        created(){
+            let currPlayer = this.currentPlayer;
+            console.log(currPlayer);
+            if(currPlayer.rolename === '狼人'){
+                currPlayer.playerimage = `/static/img/langren.jpg`;
+                currPlayer.playerskill = '';
+            }
+            else if(currPlayer.rolename === '平民'){
+                currPlayer.playerimage = `/static/img/pingming.jpg`;
+                currPlayer.playerskill = '无特殊技能，一觉睡到天亮。';
+            }
+            else if(currPlayer.rolename === '预言家'){
+                currPlayer.playerimage = `/static/img/yuyanjia.jpg`;
+                currPlayer.playerskill = '每天晚上可以查验一名玩家的身份是好人还是狼人。';
+            }
+            else if(currPlayer.rolename === '女巫'){
+                currPlayer.playerimage = `/static/img/nvwu.jpg`;
+                currPlayer.playerskill = '女巫有两瓶药，解药可以救人，毒药可以杀人.';
+            }
+            else if(currPlayer.rolename === '猎人'){
+                currPlayer.playerimage = `/static/img/lieren.jpg`;
+                currPlayer.playerskill = '猎人死的时候可以带走场上任意一名玩家';
+            }
         }
-      },
-      //查看结果
-      lookResult() {
-        this.$router.push('/gameresult')
-      },
-      //退出房间
-      outRoom(){
-        MessageBox.confirm("确定退出吗").then(action =>{
-          this.$router.back();
-        }).catch(e =>{
-        })
-      }
-    },
-    created() {   //根据角色展示技能
-      if (this.roleName === '女巫') {
-        this.roleIntro = '你有一瓶毒药和解药，解药可以救人，毒药可以杀人；</br>你一天晚上可以用两瓶药，用过解药过后就再也不知道今晚谁死了；</br>\n' +
-          '          被你毒死的玩家白天没有遗言，且毒死的猎人无法发动技能；';
-      } else if (this.roleName === '预言家') {
-        this.roleIntro = '你每天晚上可以询问上帝任意一张活人的身份，上帝会通过手势告诉你这个人是好人还是坏人。'
-      } else if (this.roleName === '猎人') {
-        this.roleIntro = "如果你不是被女巫毒死的话，你在死后可以选择发动技能带走场上任何一个人。"
-      } else if (this.roleName === '狼人') {
-        this.roleIntro = '你和你的伙伴晚上需要统一意见杀死任意一名玩家。</br>你可以在游戏的任何阶段选择自爆，自爆后，游戏将立刻中止并且直接进入天黑。</br>利用你的智慧与你的同伴一起取得胜利吧!!!'
-      } else { // 村民
-        this.roleIntro = '你只是一名普通的村民，没有任何技能。</br>利用你的智慧找出狼人吧!!！'
-      }
     }
-  }
 </script>
 
 <style scoped>
-  html, body {
-    margin: 0;
-    padding: 0;
-  }
-
-  h2 {
-    text-align: center;
-    margin: 0;
-    padding: 0;
-    font-family: "Berlin Sans FB Demi";
-  }
-
-  .back {
-    position: absolute;
-    top: 10px;
-    left: 10px;
-  }
-
-  h4 {
-    position: absolute;
-    left: 10px;
-    bottom: 10px;
-  }
-
-  .welcomePart {
-    background-image: url("../assets/wolf.jpg");
-    background-repeat: no-repeat;
-    background-position: right bottom;
-    background-size: 120px 120px;
-    color: white;
-    background-color: rgb(0, 33, 66);
-    width: 100%;
-    height: 170px;
-    margin-bottom: 5px;
+  .main{
     position: relative;
+    width: 100%;
+    height: 100%;
   }
-
-  .roleIntroduction {
-    margin-top: 40px;
-    border: 1px solid black;
-    border-radius: 5px;
-    margin-bottom: 6px;
-    margin-bottom: 40px;
+  .header{
+    background-color: #9999FF;
   }
-
-  .gameState {
-    border: 1px solid black;
-    border-radius: 5px;
-    height: 80px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  .died{
+    -webkit-filter: grayscale(100%);
+    -moz-filter: grayscale(100%);
+    -o-filter: grayscale(100%);
+    filter: grayscale(100%);
   }
-
-  #state {
+  .showotherplayer {
+    width: auto;
+    height: auto;
+    background-color: #fff;
+    border-radius: 8px;
+    border: 2px solid transparent;
+    border-image: linear-gradient(to top,#F80,#2ED);
+    border-image-slice: 10;
+    margin-bottom: 1%;
+  }
+  .showcurrentplayer{
+    height: auto;
+    width: 100%;
+    color: white;
+    background-color: rgba(0,0,0,0.7);
+  }
+  .right{
+    width: 50%;
+    float: right;
+    font-size: 1rem;
+    padding-top: 15%;
+  }
+  img{
+    width: 50%;
+  }
+  .preparing{
+    height: 30%;
+    padding-top: 10%;
+  }
+  .status{
+    height: 15%;
+  }
+  .rolename{
     color: red;
   }
-
-  .operatBtn {
-    margin-top: 40px;
+  .status>label{
+    height: 5%;
+    color: red;
   }
-
-  button {
+  .bottom{
+    position: absolute;
+    bottom: 0;
+    padding-left: 10%;
+  }
+  button{
     border-radius: 8px;
-    color: #fff;
-    background-color: rgba(0, 33, 66, 0.8);
-    width: 80px;
-    height: 36px;
-    border: none;
-    cursor: pointer;
+    background-color: #87CEFA;
+    color: white;
+  }
+  .otherPlayers {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+  .singlePlayer{
+    width: 20%;
+    height: 15%;
+    display: inline-block;
+    margin: 5%;
+  }
+  .singlePlayer>img{
+    width:100%;
+    height: 100%;
+  }
+  .skill{
+    font-size: 0.8rem;
   }
 </style>
