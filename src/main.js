27a0b06a5cb61708a74d510664f689ea3970fa7f
CatherineXWP <49479440+CatherineXWP@users.noreply.github.com>
2020-07-17 13:58:38 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import MintUI from 'mint-ui'
-import { ActionSheet } from 'vant'
+import vant from "vant"
+import 'vant/lib/index.css';
 import 'mint-ui/lib/style.css'
 import store from './store'
 import axios from 'axios'
@@ -12,7 +13,7 @@ import 'lib-flexible'
 Vue.config.productionTip = false;
 // Vue.prototype.$axios = axios;
 Vue.use(MintUI);
-Vue.use(ActionSheet);
+Vue.use(vant);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
